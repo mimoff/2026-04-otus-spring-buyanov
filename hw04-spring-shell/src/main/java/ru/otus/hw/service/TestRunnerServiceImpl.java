713527be2e625c24b49b380bd0aa6c1dd9e099df
vadim.buyanov <vadim.buyanov@example.com>
2026-07-27 00:39:06 +0300

    hw04 final

--- hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
+++ hw04-spring-shell/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -1,12 +1,11 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class TestRunnerServiceImpl implements CommandLineRunner {
+public class TestRunnerServiceImpl implements TestRunnerService {
 
     private final TestService testService;
 
@@ -14,8 +13,7 @@ public class TestRunnerServiceImpl implements CommandLineRunner {
 
     private final ResultService resultService;
 
-    @Override
-    public void run(String... args) throws Exception {
+    public void run() {
         var student = studentService.determineCurrentStudent();
         var testResult = testService.executeTestFor(student);
         resultService.showResult(testResult);
