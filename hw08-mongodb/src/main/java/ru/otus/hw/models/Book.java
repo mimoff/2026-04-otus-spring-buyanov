4f27a49479102e6b12bf2ac0e32db8a46dd447dc
vadim.buyanov <vadim.buyanov@example.com>
2026-08-04 18:58:51 +0300

    hw08 init

--- hw08-mongodb/src/main/java/ru/otus/hw/models/Book.java
+++ hw08-mongodb/src/main/java/ru/otus/hw/models/Book.java
@@ -1,26 +1,14 @@
 package ru.otus.hw.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.NamedAttributeNode;
-import jakarta.persistence.NamedEntityGraph;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.BatchSize;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.util.List;
 
@@ -28,30 +16,21 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "books")
-@Entity
-@NamedEntityGraph(name = "book-author-entity-graph", attributeNodes = {@NamedAttributeNode("author")})
-@NamedEntityGraph(name = "book-author-genres-entity-graph",
-        attributeNodes = {@NamedAttributeNode("author"), @NamedAttributeNode("genres")})
+@Document(collection = "books")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
-    private long id;
+    private String id;
 
-    @Column(name = "title")
+    @Field(name = "title")
     private String title;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
+    @DocumentReference(lazy = true)
+    @Field("author")
     private Author author;
 
-    @Fetch(FetchMode.JOIN)
-    @BatchSize(size = 10)
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "books_genres",
-            joinColumns = @JoinColumn(name = "book_id"),
-            inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    @DocumentReference(lazy = true)
+    @Field("genres")
     private List<Genre> genres;
 }
